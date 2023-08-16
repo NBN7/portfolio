91c7eddbe0ac1c336151e0721d646efd95076e5f
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-08-15 23:20:43 -0300

    fixed

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,12 +1,15 @@
 interface Props {
   children: JSX.Element;
   sectionStyle?: string;
-  sectionId: string
+  sectionId: string;
 }
 
 export const Section = ({ children, sectionStyle, sectionId }: Props) => {
   return (
-    <section id={sectionId} className={`w-full h-[100vh] p-2 overflow-hidden ${sectionStyle}`}>
+    <section
+      id={sectionId}
+      className={`w-full h-[100vh] p-2 overflow-hidden ${sectionStyle}`}
+    >
       {children}
     </section>
   );
