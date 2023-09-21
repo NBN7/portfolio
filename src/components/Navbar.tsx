fd3e515e3b64936f8fb36fa26bcc084e50385e46
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-09-20 22:47:09 -0300

    add navbar animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { MdMenu, MdClose } from "react-icons/md";
 
 export const Navbar = () => {
@@ -8,12 +9,10 @@ export const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
-  // <MdMenu onClick={handleClick} size="30px" />
-  // <MdClose onClick={handleClick} size="30px" />
   return (
     <>
       <nav className="z-50 flex md:items-center md:justify-center w-screen p-6 fixed top-0 backdrop-filter backdrop-blur-[15px]">
-        {!isOpen ? (
+        {!isOpen && (
           <div className="md:hidden">
             <MdMenu
               onClick={handleClick}
@@ -21,29 +20,38 @@ export const Navbar = () => {
               style={{ cursor: "pointer" }}
             />
           </div>
-        ) : (
-          <div className="md:hidden fixed top-0 py-6 bg-white w-full h-screen bg-inherit">
-            <MdClose
-              onClick={handleClick}
-              size="30px"
-              style={{ cursor: "pointer" }}
-            />
-            <ul className="flex flex-col text-xl gap-6 mt-10">
-              <a onClick={handleClick} href="#home">
-                <li>Home</li>
-              </a>
-              <a onClick={handleClick} href="#skills">
-                <li>Skills</li>
-              </a>
-              <a onClick={handleClick} href="#projects">
-                <li>Projects</li>
-              </a>
-              <a onClick={handleClick} href="#contact">
-                <li>Contact</li>
-              </a>
-            </ul>
-          </div>
         )}
+
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="md:hidden fixed top-0 py-6 bg-white w-full h-screen bg-inherit"
+            >
+              <MdClose
+                onClick={handleClick}
+                size="30px"
+                style={{ cursor: "pointer" }}
+              />
+              <ul className="flex flex-col text-xl gap-6 mt-10">
+                <a onClick={handleClick} href="#home">
+                  <li>Home</li>
+                </a>
+                <a onClick={handleClick} href="#skills">
+                  <li>Skills</li>
+                </a>
+                <a onClick={handleClick} href="#projects">
+                  <li>Projects</li>
+                </a>
+                <a onClick={handleClick} href="#contact">
+                  <li>Contact</li>
+                </a>
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
 
         <ul className="hidden md:flex justify-center gap-4">
           <a href="#home">
