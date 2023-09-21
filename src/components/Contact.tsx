1c52af81a15ec8b9dee46d4c7954e39170964541
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-09-20 22:57:22 -0300

    update contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,18 +3,24 @@ import { Input, Textarea } from "@nextui-org/react";
 
 export const Contact = () => {
   return (
-    <div className="w-[300px] flex flex-col gap-2 justify-center items-center">
-      <Input variant="faded" className="" type="text" label="Name" />
-      <Input variant="faded" className="" type="email" label="Email" />
+    <form
+      action="https://formspree.io/f/xnqkgzyd"
+      method="POST"
+      className="w-[300px] flex flex-col gap-2 justify-center items-center"
+    >
+      <Input variant="faded" type="text" label="Name" name="name" />
+      <Input variant="faded" type="email" label="Email" name="email" />
       <Textarea
         variant="faded"
         labelPlacement="outside"
-        placeholder="Description"
+        placeholder="Message"
+        type="text"
+        name="message"
       />
       <Button
         buttonText="Send email"
         buttonStyle="w-[300px] bg-[#242424] text-white hover:bg-[#303030] hover:transition-all hover:duration-300 p-4 rounded-lg"
       />
-    </div>
+    </form>
   );
 };
