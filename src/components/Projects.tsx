53af1a40b243b7b4136de261a8c598f51610a29e
NBN7 <nqchossj@gmail.com>
2023-10-10 16:47:18 -0300

    remove fragment

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,34 +3,32 @@ import { PROJECTS } from "../constants/projects";
 
 export const Projects = () => {
   return (
-    <>
-      <div
-        data-aos="fade-left"
-        data-aos-duration="1500"
-        className="grid grid-cols-1 md:grid-cols-2 gap-4 p-2"
-      >
-        {PROJECTS.map((project) => {
-          return (
-            <div
-              className="flex flex-col border-2 rounded-xl"
-              key={project.title}
-            >
-              <div className="flex md:w-[300px] md:h-[200px] w-[300px] h-[300px]">
-                <img
-                  className="object-cover rounded-t-xl"
-                  src={project.img}
-                  alt={project.title}
-                />
-              </div>
-              <Button
-                buttonText="Github"
-                buttonStyle="w-full bg-[#242424] text-white hover:bg-[#303030] hover:transition-all hover:duration-300 p-4 rounded-b-xl"
-                href={project.github}
+    <div
+      data-aos="fade-left"
+      data-aos-duration="1500"
+      className="grid grid-cols-1 md:grid-cols-2 gap-4 p-2"
+    >
+      {PROJECTS.map((project) => {
+        return (
+          <div
+            className="flex flex-col border-2 rounded-xl"
+            key={project.title}
+          >
+            <div className="flex md:w-[300px] md:h-[200px] w-[300px] h-[300px]">
+              <img
+                className="object-cover rounded-t-xl"
+                src={project.img}
+                alt={project.title}
               />
             </div>
-          );
-        })}
-      </div>
-    </>
+            <Button
+              buttonText="Github"
+              buttonStyle="w-full bg-[#242424] text-white hover:bg-[#303030] hover:transition-all hover:duration-300 p-4 rounded-b-xl"
+              href={project.github}
+            />
+          </div>
+        );
+      })}
+    </div>
   );
 };
